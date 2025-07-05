540b7c5b93f91cf144b4cac077aa0d4539ad8402
Nicola C. <nx9889@gmail.com>
2025-07-05 15:13:41 +0200

    fix: disabled sound emulation to avoid sound buffer overflow

--- marikoboy/mariko_boy.py
+++ marikoboy/mariko_boy.py
@@ -42,7 +42,7 @@ class Game(PyBoy):
         rom_path = join(ROMS_PATH, rom)
         if not exists(rom_path):
             raise IOError(f"File {rom_path} does not exist")
-        super().__init__(rom_path, window="null", scale=1)
+        super().__init__(rom_path, window="null", scale=1, sound_emulated=False)
         self.fps_time = time()
         self.start_time = time()
         self.rom = rom
