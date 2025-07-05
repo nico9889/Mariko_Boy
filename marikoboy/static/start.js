24c34a112ebd8106f44ca618b503e8c408f15910
Nicola C. <nx9889@gmail.com>
2025-07-05 20:32:13 +0200

    fix: removed debug alert

--- marikoboy/static/start.js
+++ marikoboy/static/start.js
@@ -1,5 +1,4 @@
 document.onreadystatechange = (state) => {
-	alert("Documento caricato correttamente");
 	console.log("docuement state:", document.readyState);
 	if (document.readyState === "complete") {
 		window.requestAnimationFrame(update);
